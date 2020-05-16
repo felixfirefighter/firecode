11c60374d5f0fb7cebc86caf73811815354855e9
Lee Yoong Ti <leeyoongti@gmail.com>
2020-05-16 18:35:45 +0800

    change replace answer

--- replace.java
+++ replace.java
@@ -2,15 +2,13 @@
 // You don't need any other imports.
 
 public static String replace(String a, String b) {
-  StringBuilder sb = new StringBuilder();
-  
-  for(int i=0;i<a.length();i++) {
-      if(a.charAt(i) == ' ') {
-          sb.append(b);
-      } else {
-          sb.append(a.charAt(i));
-      }
-  }
-  
-  return sb.toString();
+    StringBuilder sb = new StringBuilder();
+    
+    for(int i=0;i<a.length();i++) {
+        char c = a.charAt(i);
+        if(c == ' ') sb.append(b);
+        else sb.append(c);
+    }
+    
+    return sb.toString();
 }
